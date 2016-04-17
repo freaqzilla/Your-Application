4e4b1d869735c27dbebfa221e5bffb6aadd3c9f2
freaqzilla09 <snayu.arora@primepractice.com.au>
2016-04-17 05:27:08 +0000

    Added table to view

--- application/views/UserFields.php
+++ application/views/UserFields.php
@@ -5,7 +5,7 @@
         <title>Field details</title>
         <script type="text/javascript" src="<?php echo base_url() . 'assets/js/jquery-1.7.2.min.js'; ?>"></script>
         <script type="text/javascript" src="<?php echo base_url() . 'assets/js/userFieldsScript.js'; ?>"></script>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'assets/css/style.css'; ?>"">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() . 'assets/css/style.css'; ?>">
     </head>
     <body>
         <h2>Field details</h2>
@@ -37,15 +37,20 @@
         $dataNotMandatory = [
             'value' => 0
         ];
+        
 
         echo form_open('field/edit', ['class' => 'fields_form']);
+        echo '<table>';
         foreach ($userFields as $field) {
-
+            
+            echo '<tr>';
+            echo '<td>';
             echo form_label('Field Name: ');
             $dataName['value'] = $field->description;
             $dataName['name'] = 'description_' . $field->field_id;
             echo form_input($dataName);
-
+            echo '</td>';
+            echo '<td>';
             echo form_label('Field Status: ');
             $dataStatusActive['name'] = 'status_' . $field->field_id;
             $dataStatusInactive['name'] = 'status_' . $field->field_id;
@@ -59,11 +64,14 @@
             echo form_label('Active');
             echo form_radio($dataStatusActive);
             echo form_label('Inactive');
-            echo form_radio($dataStatusInactive);
 
+            echo form_radio($dataStatusInactive);
+            echo '</td>';
+            echo '<td>';
             echo form_label('Field Type: ');
             echo form_dropdown('type_' . $field->field_id, $dataType, $field->field_type);
-
+            echo '</td>';
+            echo '<td>';
             echo form_label('Field Mandatory: ');
             $dataMandatory['name'] = 'mandatory_' . $field->field_id;
             $dataNotMandatory['name'] = 'mandatory_' . $field->field_id;
@@ -78,19 +86,20 @@
             echo form_radio($dataMandatory);
             echo form_label('Not Required');
             echo form_radio($dataNotMandatory);
-
+            echo '</td>';
             $deleteButtonData = [
                 'name' => 'button',
                 'class' => 'delete_button',
                 'value' => $field->field_id,
                 'content' => 'Delete field'
             ];
+            echo '<td>';
             echo form_button($deleteButtonData);
-
-            echo '</br>';
-            echo '</br>';
+            echo '</td>';
+            echo '</tr>';
+            
         }
-        
+        echo '</table>';
         echo form_submit('save', 'Save edited fields');
 
         echo form_close();
@@ -145,10 +154,15 @@
             
             // open form
             echo form_open('field/add', ['class' => 'add_fields_form']);
+            echo '<table>';
+            echo '<tr>';
+            echo '<td>';
             
             // field name
             echo form_label('Field Name: ');
             echo form_input($dataName);
+            echo '</td>';
+            echo '<td>';
             
             // field status
             echo form_label('Field Status: ');
@@ -156,10 +170,14 @@
             echo form_radio($dataStatusActive);
             echo form_label('Inactive');
             echo form_radio($dataStatusInactive);
+            echo '</td>';
+            echo '<td>';
             
             // field type
             echo form_label('Field Type: ');
             echo form_dropdown('field_type', $dataType, 'String');
+            echo '</td>';
+            echo '<td>';
             
             // field mandatory
             echo form_label('Field Mandatory: ');
@@ -167,6 +185,8 @@
             echo form_radio($dataMandatory);
             echo form_label('Not Required');
             echo form_radio($dataNotMandatory);
+            echo '</td>';
+            echo '<td>';
             
             // save button for new field
             $saveButtonData = [
@@ -175,8 +195,10 @@
                 'content' => 'Save field data'
             ];
             echo form_button($saveButtonData);
+            echo '</td>';
             echo form_close();
-            echo '</br>';
+            echo '</tr>';
+            echo '</table>';
             ?>
         </div>
         </br>
