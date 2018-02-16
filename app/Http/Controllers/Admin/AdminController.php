d626a5d1adcf8519491357f7cc6753960cf83e32
San <freaqzilla@gmail.com>
2018-02-16 13:33:48 +1100

    Initial commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers\Controller as Controller;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class AdminController extends Controller
+{
+	public function index() {
+		var_dump('dasdasd');
+		exit;
+	}
+}
